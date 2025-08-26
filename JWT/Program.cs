55515f63e1bf3eb21d8393309cf0a840958b437d
Amr Mohamed Shawky <amrshawky936@gmail.com>
2025-08-26 20:51:35 +0300

    Add JWT authentication to ASP.NET Core application
    Implement JWT bearer token authentication with validation parameters for issuer, audience, and signing key. This enhances security for API endpoints by allowing user authentication via JWTs.

--- JWT/Program.cs
+++ JWT/Program.cs
@@ -17,6 +17,24 @@ namespace JWT
 
             builder.Services.AddDbContext<models.ApplicationDbcontext>(options =>
             options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+            builder.Services.AddAuthentication(option=>
+            {
+                option.DefaultAuthenticateScheme = "Bearer";
+                option.DefaultChallengeScheme = "Bearer";
+                option.DefaultScheme = "Bearer";})
+                .AddJwtBearer("Bearer", options =>
+                {
+                    options.SaveToken = true;
+                    options.RequireHttpsMetadata = false;
+                    options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters()
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidAudience = builder.Configuration["JWT:Audience"],
+                        ValidIssuer = builder.Configuration["JWT:Issuer"],
+                        IssuerSigningKey = new Microsoft.IdentityModel.Tokens.SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(builder.Configuration["JWT:SecretKey"]))
+                    };
+                });
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
